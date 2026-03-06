1454867867d6a3d561a315eecb127b8f57c51da2
Will Pimentel-Tonche <william.pimenteltonche@refractorsoftware.com>
2026-03-06 16:44:01 -0500

    Change array stuff

--- modules/foundation/include/foundation/containers/array.h
+++ modules/foundation/include/foundation/containers/array.h
@@ -7,55 +7,63 @@
 
 namespace hydra :: containers
 {
-
-template <typename T>
-class Array : std::pmr::vector<T>
-{
-    using Base = std::pmr::vector<T>;
-
-public:
-    explicit Array(std::pmr::memory_resource* mr = std::pmr::get_default_resource())
-    : Base(mr) {}
-
-    using Base::operator[];
-    using Base::begin;
-    using Base::end;
-
-    constexpr friend void PushBack(Array& A, T& Value)
+    template<typename T>
+    class Array : std::pmr::vector<T>
     {
-        A.push_back(Value);
-    }
+        using Base = std::pmr::vector<T>;
 
-    constexpr friend void PushBack(Array& A, const T& Value)
-    {
-        A.push_back(Value);
-    }
+    public:
+        explicit
+        Array( std::pmr::memory_resource* mr = std::pmr::get_default_resource() )
+        : Base(mr)
+        {
+        }
 
-    constexpr friend void Reserve(Array& A, u64 Count)
-    {
-        A.reserve(Count);
-    }
+        using Base::operator[];
+        using Base::begin;
+        using Base::end;
 
-    constexpr friend void Clear(Array& A)
-    {
-        A.clear();
-    }
+        constexpr friend void
+        PushBack( Array& A, T& Value )
+        {
+            A.push_back(Value);
+        }
 
-    constexpr friend bool IsEmpty(Array& A)
-    {
-        return A.empty();
-    }
+        constexpr friend void
+        PushBack( Array& A, const T& Value )
+        {
+            A.push_back(Value);
+        }
 
-    constexpr friend u64 NumElements(Array& A)
-    {
-        return A.size();
-    }
+        constexpr friend void
+        Reserve( Array& A, u64 Count )
+        {
+            A.reserve(Count);
+        }
 
-    constexpr friend void SwapRemove(Array& A, u64 Index)
-    {
-        A[Index] = std::move(A.back());
-        A.pop_back();
-    }
-};
+        constexpr friend void
+        Clear( Array& A )
+        {
+            A.clear();
+        }
 
+        constexpr friend bool
+        IsEmpty( Array& A )
+        {
+            return A.empty();
+        }
+
+        constexpr friend u64
+        NumElements( Array& A )
+        {
+            return A.size();
+        }
+
+        constexpr friend void
+        SwapRemove( Array& A, u64 Index )
+        {
+            A[Index] = std::move(A.back());
+            A.pop_back();
+        }
+    };
 }
